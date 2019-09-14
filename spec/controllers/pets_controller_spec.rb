eb019ba20c0e78031cbe4784bbbde80cf9d4c07a
kevin lang <langkevina@gmail.com>
2019-09-14 21:21:10 +0000

    Automatically backed up by Learn

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -36,7 +36,7 @@ describe "Pets Controller" do
       choose(@owner1.id)
       click_button "Create Pet"
       @pet = Pet.last
-      binding.pry
+      # binding.pry
       expect(@pet.name).to eq("Michael")
       # expect(@pet.owner.name).to eq("Cricky")
     end
@@ -48,7 +48,7 @@ describe "Pets Controller" do
       click_button "Create Pet"
       @owner = Owner.last
       @pet = Pet.last
-      binding.pry
+      # binding.pry
       expect(@pet.name).to eq("Pippa")
       expect(@pet.owner.name).to eq("Mary Nelson")
     end
