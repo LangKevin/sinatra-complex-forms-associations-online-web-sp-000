c93a910005fe243aaf21a77dfb0ba280097866da
kevin lang <langkevina@gmail.com>
2019-09-14 21:17:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,6 +28,10 @@ binding.pry
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
   patch '/pets/:id' do
 
    @pet = Pet.find(params[:id])
